7f67a9946037239fce8325993d37b7d5db33198f
Yulia Modestova <jmodestova@yahoo.com>
2019-03-11 02:33:51 +0300

    Updates

---
+++ precache-manifest.777bbc8c459b450126230654c4a3ad45.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4fa50c4003f87e7dc10459e500933c3",
+    "url": "/page-pile-game/static/media/Roboto-Medium.f4fa50c4.woff"
+  },
+  {
+    "revision": "9db8d3e8be7b86a490ab",
+    "url": "/page-pile-game/static/css/main.56724d8c.chunk.css"
+  },
+  {
+    "revision": "da4163c5b70087def745",
+    "url": "/page-pile-game/static/js/runtime~main.42401d0f.js"
+  },
+  {
+    "revision": "863e0aea86b1047ace44",
+    "url": "/page-pile-game/static/js/2.a17a3de1.chunk.js"
+  },
+  {
+    "revision": "56991a6627087ad081029dc0a64a91e4",
+    "url": "/page-pile-game/static/media/Roboto-Light.56991a66.woff"
+  },
+  {
+    "revision": "982601549987a8a6e917dcd96b2ecdeb",
+    "url": "/page-pile-game/static/media/Raleway-Bold.98260154.woff"
+  },
+  {
+    "revision": "48bd15d6cc73377c449b4027ddaa2b39",
+    "url": "/page-pile-game/static/media/Raleway-Medium.48bd15d6.woff"
+  },
+  {
+    "revision": "9db8d3e8be7b86a490ab",
+    "url": "/page-pile-game/static/js/main.5c692b37.chunk.js"
+  },
+  {
+    "revision": "ee40e9827f317314252a49be6a02364c",
+    "url": "/page-pile-game/static/media/signature_blank.ee40e982.svg"
+  },
+  {
+    "revision": "cd094dbed8c8d4edbbc56ff5d510e4b9",
+    "url": "/page-pile-game/static/media/signature.cd094dbe.svg"
+  },
+  {
+    "revision": "6771ad7a2f0ba137e68da07bf894e548",
+    "url": "/page-pile-game/static/media/type1.6771ad7a.svg"
+  },
+  {
+    "revision": "d775ec35046241f8e0bc5a7c266b10d5",
+    "url": "/page-pile-game/static/media/type2.d775ec35.svg"
+  },
+  {
+    "revision": "e61463150ea7c2b3ca1929cfb9c678ee",
+    "url": "/page-pile-game/static/media/type3_lower.e6146315.svg"
+  },
+  {
+    "revision": "a5f3b957431ed331a81a55d7a4d5eed7",
+    "url": "/page-pile-game/static/media/type3_upper.a5f3b957.svg"
+  },
+  {
+    "revision": "ba3b871e154d76cc85a99789c0cd0ff9",
+    "url": "/page-pile-game/index.html"
+  }
+];
